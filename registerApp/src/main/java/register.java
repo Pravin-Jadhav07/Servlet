c837dc045338296cbc768376b4b761b5b1ab5859
pravin jadhav <jadhavpravin7112000@gmail.com>
2024-02-05 19:04:26 +0530

    registration with servlet and jdbc

--- registerApp/src/main/java/register.java
+++ registerApp/src/main/java/register.java
@@ -7,6 +7,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 
 public class register extends HttpServlet {
@@ -16,8 +20,39 @@ public class register extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		String name = request.getParameter("name");
-		PrintWriter pw = response.getWriter();
-		pw.println("Hello " + name);
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+		String message = request.getParameter("message");
+		
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection connection = DriverManager.getConnection( "jdbc:mysql://localhost:3306/servlet", "root", "Pravin@123");
+			 PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO registration_table (name,email,password,message)"
+					 + "VALUES(?,?,?,?)" );
+			 preparedStatement.setString(1, name);
+			 preparedStatement.setString(2, email);
+			 preparedStatement.setString(3, password);
+			 preparedStatement.setString(4, message);
+			 
+			 int rowsAffected = preparedStatement.executeUpdate();
+			 
+			 PrintWriter pw = response.getWriter();
+			 
+			 if( rowsAffected != 0 ) {
+				 pw.println("<h1>Registration Successfully.</h1>");
+			 }
+			 else {
+				 pw.println("<h1>Registration failed.</h1>");
+			 }
+			 preparedStatement.close();
+			 connection.close();
+
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
 	}
 
 }
